3f4f4fbd6661d0b8fc1bb68da4061a439ffa3f9c
The Rapid Coder <md786aslam2001@gmail.com>
2024-08-26 19:58:01 +0530

    initial files is added

---
+++ portfolio.js
@@ -0,0 +1,17 @@
+/*
+Typed js--------------
+<!-- Element to contain animated typing -->
+<span id="element"></span>
+
+<!-- Load library from the CDN -->
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+
+<!-- Setup and start animation! -->
+<script>
+  var typed = new Typed('#element', {
+    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+    typeSpeed: 50,
+  });
+</script>
+</body>
+*/
